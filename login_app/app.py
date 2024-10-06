a84e59274a3edfe18698365bf62e79e710260605
iamdiniz <guilhermeplays03@gmail.com>
2024-10-06 17:16:49 -0300

    register page, transforming index to login, creating home page

--- login_app/app.py
+++ login_app/app.py
@@ -1,8 +1,28 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'daa351b615c916ff50121245a9f807b9' # Essa chave é necessária para o uso de sessões e para que o sistema de flash funcione corretamente.
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Lógica para verificar credenciais de login
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        flash('Account created successfully! Please log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
