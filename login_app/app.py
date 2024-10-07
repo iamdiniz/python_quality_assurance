eccd94ec30282f5ea68122b4928978c4e0e4a906
iamdiniz <guilhermeplays03@gmail.com>
2024-10-07 18:38:24 -0300

    adding CDN bootstrap & back to login botton & GET in users on home page

--- login_app/app.py
+++ login_app/app.py
@@ -1,6 +1,6 @@
 import re
 from functools import wraps
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
--- login_app/tests/test_app.py
+++ login_app/tests/test_app.py
@@ -1,6 +1,28 @@
+import pytest
 from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True  # Ativar modo de teste
+    with app.test_client() as client:
+        yield client
 
 def test_index_route():
     response = app.test_client().get('/login')
 
     assert response.status_code == 200
+
+def test_register_name_starts_with_number(client):
+    # Dados de teste
+    data = {
+        'name': '1Nome',
+        'email': 'teste@example.com',
+        'password': 'senha123'
+    }
+    
+    # Realiza a requisição POST para a rota /register
+    response = client.post('/register', data=data)  # Mudei para `data=data` em vez de `json=data`
+    
+    # Verifica se a resposta contém a mensagem correta
+    assert response.status_code == 400
+    assert response.get_json()['message'] == "The name cannot start with a number."
